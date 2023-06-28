618d8a0d676547d5244b253903137c7c82646c36
stefanuswisnu <42809599+stefanuswisnu@users.noreply.github.com>
2023-06-28 14:19:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split, cross_val_score
 from math import sqrt
-from matplotlib.ticker import MaxNLocator
 import pickle
 
 import os
